39feb630ce6cf225decf2eeb57726bfee03f468c
Agustín <bestwolf03@gmail.com>
2023-12-13 17:58:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// Función de modal de Cerezos
+function openModalCerezo() {
+  const modal = document.getElementById("modal-cerezo");
+  modal.style.display = "block";
+}
+
+function closeModalCerezo() {
+  const modal = document.getElementById("modal-cerezo");
+  modal.style.display = "none";
+}
+
+// Función de modal de Lirios
+
+function openModalLirio() {
+  const modal = document.getElementById("modal-lirio");
+  modal.style.display = "block";
+}
+
+function closeModalLirio() {
+  const modal = document.getElementById("modal-lirio");
+  modal.style.display = "none";
+}
+
+// Función de modal de Azaleas
+
+function openModalAzalea() {
+  const modal = document.getElementById("modal-azalea");
+  modal.style.display = "block";
+}
+
+function closeModalAzalea() {
+  const modal = document.getElementById("modal-azalea");
+  modal.style.display = "none";
+}
+
+// Función de modal de Acianos
+
+function openModalAciano() {
+  const modal = document.getElementById("modal-aciano");
+  modal.style.display = "block";
+}
+
+function closeModalAciano() {
+  const modal = document.getElementById("modal-aciano");
+  modal.style.display = "none";
+}
